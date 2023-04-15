91f9af586911ab2be26e923a3bd3610a04db464e
PabaleloLucia <48206169+PabaleloLucia@users.noreply.github.com>
2023-04-15 18:25:27 +0200

    defined date elements and displayed

--- src/index.js
+++ src/index.js
@@ -1,19 +1,47 @@
+function actualDate(timestamp) {
+  let date = new Date(timestamp);
+  let hours = date.getHours();
+  if (hours < 10) {
+    hours = `0${hours}`;
+  }
+
+  let minutes = date.getMinutes();
+
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  let day = days[date.getDay()];
+  return `${day} ${hours}:${minutes}`;
+}
+
 function showTemperature(response) {
-  console.log(response.data);
   let tempElement = document.querySelector("#temperature");
   let cityElement = document.querySelector("#city");
   let descriptionElement = document.querySelector("#description");
   let humidityElement = document.querySelector("#humidity");
   let windElement = document.querySelector("#wind");
+  let dateElement = document.querySelector("#date");
 
   tempElement.innerHTML = Math.round(response.data.main.temp);
   cityElement.innerHTML = response.data.name;
   descriptionElement.innerHTML = response.data.weather[0].description;
   humidityElement.innerHTML = Math.round(response.data.main.humidity);
   windElement.innerHTML = Math.round(response.data.wind.speed);
+  dateElement.innerHTML = actualDate(response.data.dt * 1000);
 }
 
 let apiKey = "44901ff0e09df2c286ef98d3d56d16ff";
-let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=Gaborone&appid=${apiKey}&units=metric`;
+let city = "Gaborone";
+let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
 
 axios.get(apiUrl).then(showTemperature);
